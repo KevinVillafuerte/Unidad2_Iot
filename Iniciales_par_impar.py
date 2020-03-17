6a2628747149e501c5df9dd3c3e678b2967962b6
KevinVillafuerte <56412490+KevinVillafuerte@users.noreply.github.com>
2020-03-17 17:31:30 -0600

    Delete Iniciales_par_impar.py

--- Iniciales_par_impar.py
+++
@@ -1,7 +0,0 @@
-#Iniciales_par_impar.py
-class NumerosParImpar():
-    a=int(input("ingrese un numero: "))
-    if(a%2==0):
-        print(str(a)+" Es par")
-    else:
-        print(str(a)+" Es impar")
